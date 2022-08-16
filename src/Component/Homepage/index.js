626838ef4dcb21ece6c60505cd0672c0a0007aeb
Nguyễn Trung Tài <54946482+papershoot@users.noreply.github.com>
2022-08-16 16:59:13 +0700

    Update index.js

--- src/Component/Homepage/index.js
+++ src/Component/Homepage/index.js
@@ -76,14 +76,14 @@ function Home() {
                             }}>
                             <p>Hi, {sessionStorage.getItem("Username")}</p>
 
-                            <Avatar
-                                src={require("file:///D:/react/vachay_web/src/asset/images/logo.jpg")}
-                                sx={{
-                                    width: '100px',
-                                    height: '100px',
-                                    marginLeft: '75px'
+//                             <Avatar
+//                                 src={require("file:///D:/react/vachay_web/src/asset/images/logo.jpg")}
+//                                 sx={{
+//                                     width: '100px',
+//                                     height: '100px',
+//                                     marginLeft: '75px'
 
-                                }} />
+//                                 }} />
 
                         </Box>
 
